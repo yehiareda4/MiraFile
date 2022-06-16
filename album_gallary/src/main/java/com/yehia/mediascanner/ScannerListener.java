03d2f59a15ce9cb0997483c58dcc0fb9e350b6ab
yehia <yehiareda376@gamil.com>
2022-06-16 15:43:58 +0200

    new v

--- album_gallary/src/main/java/com/yehia/mediascanner/ScannerListener.java
+++ album_gallary/src/main/java/com/yehia/mediascanner/ScannerListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © Yehia Reda. All Rights Reserved
+ * Copyright © Yan Zhenjie. All Rights Reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,17 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.yehia.mediascanner
+package com.yehia.mediascanner;
 
-import android.net.Uri
+import android.net.Uri;
 
 /**
- *
- * Scanner Listener.
+ * <p>Scanner Listener.</p>
  * Created by YanZhenjie on 17-3-27.
+ *
+ * @deprecated not use.
  */
-@Deprecated("")
-interface ScannerListener {
-    fun oneComplete(path: String, uri: Uri)
-    fun allComplete(filePaths: Array<String>)
+@Deprecated
+public interface ScannerListener {
+
+    void oneComplete(String path, Uri uri);
+
+    void allComplete(String[] filePaths);
+
 }
